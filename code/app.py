956a4b34c6696f6376901b6771576d39c843c998
AlonsoMD <alonso2295@hotmail.com>
2019-12-15 11:36:02 -0500

    complete CRUD

--- code/app.py
+++ code/app.py
@@ -1,69 +1,34 @@
 from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Api
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
 from user import UserRegister
+from item import Item, ItemList
 
 app = Flask(__name__)
 app.secret_key = 'jose'
 api = Api(app)
 
+
+# https://blog.tecladocode.com/learn-python-advanced-configuration-of-flask-jwt/
+app.config["JWT_AUTH_URL_RULE"] = '/login'
 jwt = JWT(app,authenticate,identity) # create new endpoint /auth
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=3600)
+#app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
 
-items = []
+#@jwt.auth_response_handler
+#def customized_response_handler(access_token, identity):
+#    return jsonify({
+#                        'access_token': access_token.decode('utf-8'),
+#                        'user_id': identity.id
+#                   })
 
-class Item(Resource):
-
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',
-                    type = float,
-                    required = True,
-                    help = "This field cannot be left blank!"
-                    )
-
-    @jwt_required()
-    def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None) # next give us the first item matched 
-        return {'item': item} , 200 if item else 404
-
-    def post(self,name):
-        #Error control if user add a name that already exists
-        if next(filter(lambda x: x['name'] == name, items), None) is not None:
-            return {'message': "An item with name '{}' already exists.".format(name)}, 400
-        
-        data = Item.parser.parse_args()
-        #data = request.get_json() #force = True to transform content type to json, silent = True it doesnt give an error return none
-
-        item = {'name': name, 'price':data["price"]}
-        items.append(item)
-        return item, 201
-    
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items)) # we overwrite the list without the element deleted
-        return {'message': 'Item deleted'}
-
-    def put(self, name):
-        data = Item.parser.parse_args()
-        #data = request.get_json()
-
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name':name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
-
-
-class ItemList(Resource):
-    def get(self):
-        return {'items':items}
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
-app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
 
--- code/create_tables.py
+++ code/create_tables.py
@@ -5,7 +5,11 @@ cursor = connection.cursor()
  
 create_table = "CREATE TABLE IF NOT EXISTS USERS (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_table)
- 
+
+
+create_table = "CREATE TABLE IF NOT EXISTS ITEMS (name text, price real)"
+cursor.execute(create_table)
+
 connection.commit()
 connection.close()
 
